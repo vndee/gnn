818a02054bcc4d1f71d19ea1c484ebd34fbc5b77
Duy Huynh <hvd.huynhduy@gmail.com>
2020-03-14 00:20:24 +0700

    update GCN

--- gcn.py
+++ gcn.py
@@ -1,10 +1,15 @@
 import dgl
+import time
 import torch
+import numpy as np
+import networkx as nx
 import torch.nn as nn
 import dgl.function as fn
 import torch.nn.functional as F
+import matplotlib.pyplot as plt
 
 from dgl import DGLGraph
+from dgl.data import citation_graph
 
 gcn_msg = fn.copy_src(src='h', out='m')
 gcn_reduce = fn.sum(msg='m', out='h')
@@ -47,5 +52,58 @@ class GCNNet(nn.Module):
         return x
 
 
+def load_cora_data():
+    data = citation_graph.load_cora()
+    features = torch.FloatTensor(data.features)
+    labels = torch.LongTensor(data.labels)
+    train_mask = torch.BoolTensor(data.train_mask)
+    test_mask = torch.BoolTensor(data.test_mask)
+    g = data.graph
+
+    g.remove_edges_from(nx.selfloop_edges(g))
+    g = DGLGraph(g)
+    g.add_edges(g.nodes(), g.nodes())
+
+    figs, ax = plt.subplots()
+    nx.draw(g.to_networkx(), ax=ax)
+    ax.set_title('Cora citation graph')
+    plt.show()
+    return g, features, labels, train_mask, test_mask
+
+
+def evaluate(model, g, features, labels, mask):
+    model.eval()
+    with torch.no_grad():
+        logits = model(g, features)
+        logits = logits[mask]
+        labels = labels[mask]
+        _, indices = torch.max(logits, dim=1)
+        correct = torch.sum(indices == labels)
+        return correct.item() * 1.0 / len(labels)
+
+
 net = GCNNet()
 print(net)
+
+g, features, labels, train_mask, test_mask = load_cora_data()
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-3)
+dur = []
+
+for epoch in range(50):
+    if epoch >= 3:
+        t0 = time.time()
+
+    net.train()
+    logits = net(g, features)
+    log = F.log_softmax(logits, 1)
+    loss = F.nll_loss(log[train_mask], labels[train_mask])
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if epoch >= 3:
+        dur.append(time.time() - t0)
+
+    acc = evaluate(net, g, features, labels, test_mask)
+    print(f'Epoch: {epoch} | Loss: {round(loss.item(), 4)} | Accuracy: {round(acc, 4)} | Time(s): {np.mean(dur)}')
